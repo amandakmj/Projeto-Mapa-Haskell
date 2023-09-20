f60ef2274ea609208a2a4b499c850c743c5d3a1c
Amanda <amandasilvaraujo@outlook.com>
2023-09-20 17:26:44 -0300

    Finalizado parte 1

--- proj.hs
+++ proj.hs
@@ -15,7 +15,7 @@ mapaInicial = []
 
 -- Adiciona uma cidade ao mapa
 addCidade :: Mapa.Mapa -> Mapa.Cidade -> Mapa.Mapa
-addCidade mapa cidade = cidade : mapa
+addCidade mapa cidade =  cidade : mapa 
 
 atualizarEstradasRemovidas :: Mapa.Mapa -> Mapa.Nome -> Mapa.Mapa
 atualizarEstradasRemovidas [] _ = []
@@ -45,12 +45,23 @@ addEstrada mapa cidadeOrigem cidadeDestino =
 removeEstrada :: Mapa.Mapa -> Mapa.Nome -> Mapa.Nome -> Mapa.Mapa
 removeEstrada mapa cidadeOrigem cidadeDestino =
     [(nome, coordenadas, if nome == cidadeOrigem then [estrada | estrada <- estradas, estrada /= cidadeDestino] else 
-                        if nome == cidadeDestino then[estrada | estrada <- estradas, estrada /= cidadeDestino]  else estradas) |
+                        if nome == cidadeDestino then[estrada | estrada <- estradas, estrada /= cidadeOrigem]  else estradas) |
     (nome, coordenadas, estradas) <- mapa]
 
 -- Função auxiliar para verificar se uma cidade existe no mapa
 cidadeExiste :: Mapa.Nome -> Mapa.Mapa -> Bool
 cidadeExiste cidade mapa = elem cidade [nome | (nome, _, _) <- mapa]
+
+coordenadasExistem :: Coordenadas -> Mapa.Mapa -> Bool
+coordenadasExistem coordenadas mapa = elem coordenadas [(x, y) | (_, (x, y), _) <- mapa]
+
+-- Verifica se duas cidades possuem conexão
+estradaExiste :: Mapa.Mapa -> Mapa.Nome -> Mapa.Nome -> Bool
+estradaExiste mapa cidadeOrigem cidadeDestino =
+    let estradasOrigem = head [estradas | (nome, _, estradas) <- mapa, nome == cidadeOrigem]
+        estradasDestino = head [estradas | (nome, _, estradas) <- mapa, nome == cidadeDestino]
+    in cidadeDestino `elem` estradasOrigem || cidadeOrigem `elem` estradasDestino
+
 
 -- Função para obter o nome de uma cidade do teclado, verificando se a cidade existe no mapa
 checarCidade :: Mapa.Mapa -> IO Mapa.Nome
@@ -62,6 +73,28 @@ checarCidade mapa = do
         else do
             putStrLn "Cidade não encontrada. Tente novamente."
             checarCidade mapa
+
+--Função que checa se a cidade que o usuário digitar ja existe no mapa,de maneira que não possam haver mais de uma cidade com o mesmo nome
+checarCidadeRepetida :: Mapa.Mapa -> IO Mapa.Nome 
+checarCidadeRepetida  mapa= do
+    
+    nome <- getLine        
+    if cidadeExiste nome mapa == False
+        then return nome
+         else do
+            putStrLn "Esta cidade já existe. Tente novamente."
+            checarCidadeRepetida mapa
+--Função que checa se as coordenadas digitadas pelo usuário já fazem parte de uma cidade existente, pois duas cidades não podem ocupar o mesmo espaço
+checarCoordenadasRepetidas :: Mapa.Mapa -> IO Coordenadas
+checarCoordenadasRepetidas mapa = do
+    coordenadasStr <- getLine
+    let [x, y] = map read (words coordenadasStr)
+    if coordenadasExistem (x, y) mapa
+        then do
+            putStrLn "Essas coordenadas já existem. Tente novamente."
+            checarCoordenadasRepetidas mapa
+        else return (x, y)
+
 
 -- Função principal para manipular o mapa
 manipularMapa :: Mapa.Mapa -> IO ()
@@ -80,43 +113,66 @@ manipularMapa mapa = do
     case opcao of
         "1" -> do
             putStrLn "Digite o nome da cidade que você deseja adicionar:"
-            nomeCidade <- getLine
+            nomeCidade <- checarCidadeRepetida mapa
     
             putStrLn "Digite as coordenadas (x,y) da cidade (separadas por espaço):"
-            coordenadasStr <- getLine
-            let [x, y] = map read (words coordenadasStr)
+            (x, y) <- checarCoordenadasRepetidas mapa
     
             let cidadeAdicionada = (nomeCidade, (x, y), [])
             manipularMapa (addCidade mapa cidadeAdicionada)
-        "2" -> do
-            putStrLn "Digite o nome da cidade a remover:"
-            nome <- checarCidade mapa
-            manipularMapa (removeCidade mapa nome)
-        "3" -> do
+        "2" ->
+            if length mapa == 0 
+                then do
+                    putStrLn "Nao eh possivel remover uma cidade de um mapa vazio."
+                    manipularMapa mapa
+            else do
+                putStrLn "Digite o nome da cidade a remover:"
+                nome <- checarCidade mapa
+                manipularMapa (removeCidade mapa nome)
+        "3" -> 
+            if length mapa == 0 
+                then do
+                    putStrLn "Nao eh possivel adicionar uma estrada a um mapa vazio."
+                    manipularMapa mapa
+            else do
             putStrLn "Digite o nome da cidade origem:"
             origem <- checarCidade mapa
             putStrLn "Digite o nome da cidade destino:"
             destino <- checarCidade mapa
             manipularMapa (addEstrada mapa origem destino)
 
-        "4" -> do
+        "4" -> 
+            if length mapa == 0 
+                then do
+                    putStrLn "Nao eh possivel remover uma estrada de um mapa vazio."
+                    manipularMapa mapa
+            else do        
             putStrLn "Digite o nome da cidade origem:"
             origem <- checarCidade mapa
             putStrLn "Digite o nome da cidade destino:"
             destino <- checarCidade mapa
-            manipularMapa (removeEstrada mapa origem destino)
+            if estradaExiste mapa origem destino
+                then do
+                    manipularMapa (removeEstrada mapa origem destino)
+            else do
+                putStrLn "As cidades estao desconectadas"
+                manipularMapa mapa
 
 
         "5" -> do 
+            putStrLn "Digite o nome do mapa que voce deseja salvar(com a extensao.mapa)."
+            nomeMapa <- getLine
             putStrLn "Salvando mapa..."
-            salvouComSucesso <- salvarMapa mapa "cidades.mapa"
+            salvouComSucesso <- salvarMapa mapa nomeMapa
             if salvouComSucesso
                 then manipularMapa mapa  -- Se salvou com sucesso, continua com o mapa atual
                 else putStrLn "Erro ao salvar o mapa."  -- Caso contrário, exibe uma mensagem de erro
 
         "6" -> do
+            putStrLn "Digite o nome do mapa que voce deseja carregar(com a extensao.mapa)."
+            nomeMapa <- getLine
             putStrLn "Carregando mapa"
-            mapa <- carregarMapa "cidades.mapa"
+            mapa <- carregarMapa nomeMapa
             manipularMapa mapa 
         "7" -> putStrLn "Saindo..."
         _ -> do
